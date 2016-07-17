2d282882c266f96cdb5d84d5e317afe0713df79d
Murli Hiware <murlihiware@gmail.com>
2016-07-17 17:18:50 +0530

    search in Binary Tree

--- src/com/ds/tree/BinaryTree.java
+++ src/com/ds/tree/BinaryTree.java
@@ -11,7 +11,8 @@ import java.util.concurrent.ConcurrentLinkedQueue;
  */
 public class BinaryTree {
 
-	static int count=0;
+	static int count = 0;
+
 	/**
 	 * 
 	 * @param args
@@ -19,26 +20,24 @@ public class BinaryTree {
 	public static void main(String[] args) {
 
 		BinaryTreeNode root = createBinaryTree();
-		int maxValue = findMaxInBinaryTree(root);
-		System.out.println("Max Value:"+maxValue);
+		boolean searchResult = searchBinaryTree(root, 10);
+		System.out.println("Search Result:" + searchResult);
 	}
-	
-	public static BinaryTreeNode createBinaryTree()
-	{
+
+	public static BinaryTreeNode createBinaryTree() {
 		BinaryTreeNode root = new BinaryTreeNode(1);
 		root.leftChild = new BinaryTreeNode(4);
 		root.rightChild = new BinaryTreeNode(2);
-		
+
 		root.leftChild.leftChild = new BinaryTreeNode(6);
 		root.leftChild.rightChild = new BinaryTreeNode(5);
-		
+
 		root.rightChild.leftChild = new BinaryTreeNode(3);
 		root.rightChild.rightChild = new BinaryTreeNode(0);
-	    return root;
+		return root;
 	}
-	
-	public static int findMaxInBinaryTree(BinaryTreeNode root)
- {
+
+	public static int findMaxInBinaryTree(BinaryTreeNode root) {
 		int maxValue = Integer.MIN_VALUE;
 		if (root == null)
 			return maxValue;
@@ -53,172 +52,150 @@ public class BinaryTree {
 		return maxValue;
 	}
 	
-	public static BinaryTreeNode createBinarySearchTree()
-	{
+	public static boolean searchBinaryTree(BinaryTreeNode root, int key) {
+		
+		if(root==null)
+			return false;
+		if(root.data==key)
+			return true;
+		return searchBinaryTree(root.leftChild, key) || searchBinaryTree(root.rightChild, key);
+	}
+
+	public static BinaryTreeNode createBinarySearchTree() {
 		BinaryTreeNode root = new BinaryTreeNode(10);
 		root.leftChild = new BinaryTreeNode(6);
 		root.rightChild = new BinaryTreeNode(14);
-		
+
 		root.leftChild.leftChild = new BinaryTreeNode(4);
 		root.leftChild.rightChild = new BinaryTreeNode(8);
-		
+
 		root.rightChild.leftChild = new BinaryTreeNode(12);
 		root.rightChild.rightChild = new BinaryTreeNode(16);
-	    return root;
+		return root;
 	}
-	
-	
-	public static void addElement(int data)
-	{
-		
+
+	public static void addElement(int data) {
+
 	}
-	
-	public static void inorderDisplay(BinaryTreeNode root)
-	{
-		if(root!=null)
-		{
+
+	public static void inorderDisplay(BinaryTreeNode root) {
+		if (root != null) {
 			inorderDisplay(root.rightChild);
 			System.out.println(root.data);
 			inorderDisplay(root.leftChild);
 		}
-		
+
 	}
-	
-	public static void nonRecursiveInorderDisplay(BinaryTreeNode root)
-	{
-		if(root==null)
+
+	public static void nonRecursiveInorderDisplay(BinaryTreeNode root) {
+		if (root == null)
 			return;
-		
+
 		Stack<BinaryTreeNode> s = new Stack<>();
 		BinaryTreeNode currentNode = root;
-		
-		while(true)
-		{
-			if(currentNode!=null)
-			{
+
+		while (true) {
+			if (currentNode != null) {
 				s.push(currentNode);
 				currentNode = currentNode.rightChild;
-			}
-			else
-			{
-				if(!s.isEmpty())
-				{
-					currentNode=s.pop();
-					System.out.println("Data:"+currentNode.data);
-						currentNode = currentNode.leftChild;
-				}
-				else
-				{
+			} else {
+				if (!s.isEmpty()) {
+					currentNode = s.pop();
+					System.out.println("Data:" + currentNode.data);
+					currentNode = currentNode.leftChild;
+				} else {
 					break;
 				}
-				
+
 			}
 		}
 	}
-	
-	public static BinaryTreeNode nonRecursiveInorderDisplay(BinaryTreeNode root, int k)
-	{
-		if(root==null)
+
+	public static BinaryTreeNode nonRecursiveInorderDisplay(BinaryTreeNode root, int k) {
+		if (root == null)
 			return null;
-		int count=0;
+		int count = 0;
 		Stack<BinaryTreeNode> s = new Stack<>();
 		BinaryTreeNode currentNode = root;
-		
-		while(true)
-		{
-			if(currentNode!=null)
-			{
+
+		while (true) {
+			if (currentNode != null) {
 				s.push(currentNode);
 				currentNode = currentNode.rightChild;
-			}
-			else
-			{
-				if(!s.isEmpty())
-				{
-					currentNode=s.pop();
-				    count++;
-				    if(count==k)
-				    	return currentNode;
+			} else {
+				if (!s.isEmpty()) {
+					currentNode = s.pop();
+					count++;
+					if (count == k)
+						return currentNode;
 					currentNode = currentNode.leftChild;
-				}
-				else
-				{
+				} else {
 					break;
 				}
-				
+
 			}
 		}
-		
+
 		return null;
 	}
-	
-	public static int kthNode(BinaryTreeNode root, int k)
- {
+
+	public static int kthNode(BinaryTreeNode root, int k) {
 		if (root == null)
 			return -1;
-		else
-		{
-			int result = kthNode(root.rightChild, k);	
-			if(result!=-1)
+		else {
+			int result = kthNode(root.rightChild, k);
+			if (result != -1)
 				return result;
 			count++;
-			if(count==k)
+			if (count == k)
 				return root.data;
 			return kthNode(root.leftChild, k);
 		}
-		
+
 	}
-	
-	
-	public static void levelOrderDisplay(BinaryTreeNode root)
-	{
-		if(root==null)
+
+	public static void levelOrderDisplay(BinaryTreeNode root) {
+		if (root == null)
 			return;
 		ArrayList<Integer> arrayList = new ArrayList<>();
 		Queue<BinaryTreeNode> queue = new ConcurrentLinkedQueue<>();
 		BinaryTreeNode tempNode;
-		int count=0;
+		int count = 0;
 		BinaryTreeNode nullNode = new BinaryTreeNode(-1234);
 		queue.offer(root);
 		queue.offer(nullNode);
 
-		while(!queue.isEmpty())
-		{
+		while (!queue.isEmpty()) {
 			tempNode = queue.poll();
-			if(tempNode!=nullNode)
-			{
+			if (tempNode != nullNode) {
 				arrayList.add(tempNode.data);
-				if(tempNode.leftChild!=null)
+				if (tempNode.leftChild != null)
 					queue.offer(tempNode.leftChild);
-				if(tempNode.rightChild!=null)
+				if (tempNode.rightChild != null)
 					queue.offer(tempNode.rightChild);
 			}
-			
-			else
-			{
-				System.out.print("Level:"+(count++)+"=>");
-				arrayList.stream().forEach(a->System.out.print(a+" "));
+
+			else {
+				System.out.print("Level:" + (count++) + "=>");
+				arrayList.stream().forEach(a -> System.out.print(a + " "));
 				System.out.println();
 				arrayList.clear();
-				if(!queue.isEmpty())
+				if (!queue.isEmpty())
 					queue.offer(nullNode);
 			}
 		}
-		
-		
+
 	}
 
 }
 
-
-class BinaryTreeNode
-{
+class BinaryTreeNode {
 	public int data;
 	public BinaryTreeNode leftChild;
 	public BinaryTreeNode rightChild;
+
 	public BinaryTreeNode(int data) {
 		this.data = data;
 	}
-	
-	
+
 }
